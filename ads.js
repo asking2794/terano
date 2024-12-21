7cd0139f9dff8a16f4ada00bf008e16934c9d986
asking2794 <146243355+asking2794@users.noreply.github.com>
2024-12-21 20:43:12 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,8 @@
+ atOptions = {
+		'key' : 'c0e7ae62487c201244859c735cc89d8a',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c0e7ae62487c201244859c735cc89d8a/invoke.js"></scr' + 'ipt>');
